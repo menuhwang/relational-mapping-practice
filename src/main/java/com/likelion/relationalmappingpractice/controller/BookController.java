f54b512801e46de267e37ab406a7068482390d33
menuHwang <hmin0206@naver.com>
2022-11-23 12:52:35 +0900

    [Feat] BookController id로 조회

---
+++ src/main/java/com/likelion/relationalmappingpractice/controller/BookController.java
@@ -0,0 +1,24 @@
+package com.likelion.relationalmappingpractice.controller;
+
+import com.likelion.relationalmappingpractice.domain.entity.Book;
+import com.likelion.relationalmappingpractice.service.BookService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class BookController {
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> get(@PathVariable("id") Integer id) {
+        return ResponseEntity.ok(bookService.get(id));
+    }
+}
